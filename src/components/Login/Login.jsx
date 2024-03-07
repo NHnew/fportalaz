ed2e4738536cf8109ba29e4c12c74a342bee1523
NHnew nabi <nbihuseynov1@gmail.com>
2024-03-07 13:25:21 +0000

    changessss

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Logo from '../assets/BIGfavicon.png';
+import React, { useState } from 'react';
+import Logo from '../assets/fportal.webp';
 import { FcGoogle } from "react-icons/fc";
 import { FaApple } from "react-icons/fa";
 
@@ -7,39 +7,77 @@ import { FaApple } from "react-icons/fa";
 
 
 const Login = () => {
+
+    const [action, setAction] = useState('Qeydiyyatdan keç!');
+
+    const actionChnage = () => action === 'Qeydiyyatdan keç!' ? setAction('Giriş et!') : setAction('Qeydiyyatdan keç!');
+
     return (
         <div className='sign flex w-full min-h-screen'>
-            <div className='register w-1/2 text-white bg-black px-10 py-5'>
-                <img className='logo w-28' src={Logo} alt="" />
-                <h1 className='signtitle text-4xl mt-12 mb-10'>Futbol dünyasına <span className='text-fuchsia-600'>keçid et!</span></h1>
-                <form className='max-w-[410px]' action="Get">
-                    <div className='mb-5'>
-                        <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="email" placeholder='username or email' required />
-                    </div>
-                    <div>
-                        <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="password" placeholder='password' required />
-                    </div>
-                    <div className='mt-5 flex items-center justify-between'>
-                        <button className='loginbtn bg-white text-black px-5 py-2 font-bold rounded-3xl'>giriş et</button>
-                        <span className='parolforget text-fuchsia-600 text-sm cursor-pointer'>şifrəni unutdun?</span>
-                    </div>
-                </form>
-                <div className='signfooter max-w-[410px] mt-16 border-t-2 border-gray-300'>
-                    <div className='flex justify-center'>
-                        <div className='otherway bg-black -mt-3 px-3'>Digər üsullar</div>
-                    </div>
-                    <div className='flex justify-between mt-4'>
-                        <div className='googlebtn flex items-center justify-center bg-zinc-900 px-7 py-1 rounded-3xl '>
-                            <FcGoogle />
-                            <button className='ml-2 text-slate-400'>Google ilə giriş</button>
+            <div className='register flex justify-center items-center w-1/2 text-white bg-black px-10 py-5'>
+                <div>
+                    <img className='logo w-48' src={Logo} alt="" />
+                    <h1 className='signtitle text-4xl mt-12 mb-10'>Futbol dünyasına <span className='samecolor text-fuchsia-600'>keçid et!</span></h1>
+                    <form className='max-w-[410px]' action="Get">
+                        {
+                            action === 'Qeydiyyatdan keç!' ? <div>
+                                <div className='mb-5'>
+                                    <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="email" placeholder='istifadəçi adı və ya email' required />
+                                </div>
+                                <div>
+                                    <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="password" placeholder='şifrə' required />
+                                </div>
+                            </div> : <div>
+                                <div className='mb-5'>
+                                    <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="text" placeholder='ad soyad' required />
+                                </div>
+                                <div className='mb-5'>
+                                    <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="text" placeholder='istifadəçi adı' required />
+                                </div>
+                                <div className='mb-5'>
+                                    <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="email" placeholder='email' required />
+                                </div>
+                                <div>
+                                    <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="password" placeholder='şifrə' required />
+                                </div>
+                            </div>
+                        }
+
+                        <div className='mt-5 flex items-center justify-between'>
+                            {
+                                action === 'Qeydiyyatdan keç!' ? <button className='loginbtn bg-white text-black px-5 py-2 font-bold rounded-3xl'>giriş et</button> : <button className='loginbtn bg-white text-black px-5 py-2 font-bold rounded-3xl'>qeydiyyat</button>
+                            }
+                            {
+                                action === 'Qeydiyyatdan keç!' ? <span className='samecolor parolforget text-fuchsia-600 text-sm cursor-pointer'>şifrəni unutdun?</span> : null
+                            }
                         </div>
-                        <div className='applebtn flex items-center justify-center bg-zinc-900 px-7 py-1 rounded-3xl'>
-                            <FaApple />
-                            <button className='ml-2 text-slate-400'>Apple ilə giriş</button>
+                    </form>
+                    <div className='signfooter max-w-[410px] mt-16 border-t-2 border-gray-300'>
+                        <div className='flex justify-center'>
+                            <div className='otherway bg-black -mt-3 px-3'>Digər üsullar</div>
                         </div>
-                    </div>
-                    <div>
-                        <p className='endtext text-sm mt-4'>Hesabınız yoxdur? <span className='text-fuchsia-600 cursor-pointer'>Qeydiyyatdan keç!</span> </p>
+                        <div className='flex justify-between mt-4'>
+                            <div className='googlebtn flex items-center justify-center bg-zinc-900 mr-2 px-7 py-2 rounded-3xl '>
+                                <FcGoogle />
+                                {
+                                    action === 'Qeydiyyatdan keç!' ? <button className='ml-2 text-slate-400'>Google ilə giriş</button> : <button className='ml-2 text-slate-400'>Google ilə qeydiyyat</button>
+                                }
+                            </div>
+                            <div className='applebtn flex items-center justify-center bg-zinc-900 px-7 py-2 rounded-3xl'>
+                                <FaApple />
+                                {
+                                    action === 'Qeydiyyatdan keç!' ? <button className='ml-2 text-slate-400'>Apple ilə giriş</button> : <button className='ml-2 text-slate-400'>Apple ilə qeydiyyat</button>
+                                }
+                            </div>
+                        </div>
+                        <div>
+                            <p className='endtext text-sm mt-4'>
+                                {
+                                    action === 'Qeydiyyatdan keç!' ? <span className='mr-1'>Hesabınız yoxdur?</span> : <span className='mr-1'>Hesabınız var?</span>
+                                }
+                                <span onClick={actionChnage} className='samecolor text-fuchsia-600 cursor-pointer'>{action}</span>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -50,3 +88,5 @@ const Login = () => {
 };
 
 export default Login;
+
+
