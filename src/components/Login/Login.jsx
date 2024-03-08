1b7acf4c1d98a5cdd1c66254b9ac81ad3e3326e4
NHnew nabi <nbihuseynov1@gmail.com>
2024-03-08 12:30:45 +0000

    loginchanges

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -14,27 +14,27 @@ const Login = () => {
 
     return (
         <div className='sign flex w-full min-h-screen'>
-            <div className='register flex justify-center items-center w-1/2 text-white bg-black px-10 py-5'>
+            <div className='register flex flex justify-center items-center w-1/2 text-white bg-black px-10 py-5'>
                 <div>
                     <img className='logo w-48' src={Logo} alt="" />
                     <h1 className='signtitle text-4xl mt-12 mb-10'>Futbol dünyasına <span className='samecolor text-fuchsia-600'>keçid et!</span></h1>
                     <form className='max-w-[410px]' action="Get">
                         {
                             action === 'Qeydiyyatdan keç!' ? <div>
-                                <div className='mb-5'>
+                                <div className='mb-4'>
                                     <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="email" placeholder='istifadəçi adı və ya email' required />
                                 </div>
                                 <div>
                                     <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="password" placeholder='şifrə' required />
                                 </div>
                             </div> : <div>
-                                <div className='mb-5'>
+                                <div className='mb-4'>
                                     <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="text" placeholder='ad soyad' required />
                                 </div>
-                                <div className='mb-5'>
+                                <div className='mb-4'>
                                     <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="text" placeholder='istifadəçi adı' required />
                                 </div>
-                                <div className='mb-5'>
+                                <div className='mb-4'>
                                     <input className='w-full px-3 py-2 bg-zinc-900 rounded-3xl' type="email" placeholder='email' required />
                                 </div>
                                 <div>
@@ -43,7 +43,7 @@ const Login = () => {
                             </div>
                         }
 
-                        <div className='mt-5 flex items-center justify-between'>
+                        <div className='mt-4 flex items-center justify-between'>
                             {
                                 action === 'Qeydiyyatdan keç!' ? <button className='loginbtn bg-white text-black px-5 py-2 font-bold rounded-3xl'>giriş et</button> : <button className='loginbtn bg-white text-black px-5 py-2 font-bold rounded-3xl'>qeydiyyat</button>
                             }
@@ -71,7 +71,7 @@ const Login = () => {
                             </div>
                         </div>
                         <div>
-                            <p className='endtext text-sm mt-4'>
+                            <p className='endtext text-sm mt-3'>
                                 {
                                     action === 'Qeydiyyatdan keç!' ? <span className='mr-1'>Hesabın yoxdur?</span> : <span className='mr-1'>Hesabın var?</span>
                                 }
